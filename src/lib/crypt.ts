3e5b6f29351252a5659331fce1611f6db6806f19
Rendall <rendall@gmail.com>
2020-11-08 23:46:43 +0200

    Add uuid

--- src/lib/crypt.ts
+++ src/lib/crypt.ts
@@ -18,3 +18,6 @@ export const comparePassword = (plainTextPassword: string, hash: string) => bcry
   .compare(plainTextPassword, hash)
 
 export const getAuthToken = (user:string, exp:number = getExpirationTime(DAY)) => jwt.sign({user, exp}, process.env.JWT_SECRET)
+
+//@ts-ignore
+export const uuidv4 = () => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)); // https://gist.github.com/LeverOne/1308368
