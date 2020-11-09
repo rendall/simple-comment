d0d5f08e1c5e9b1bbe23f382067684a57aff7a73
Rendall <rendall@gmail.com>
2020-11-09 08:26:54 +0200

    Refine secrets.test.ts

--- tests/secrets.test.ts
+++ tests/secrets.test.ts
@@ -2,21 +2,36 @@ import * as dotenv from "dotenv"
 import * as fs from "fs"
 dotenv.config()
 
-let lines: string[]
+let exampleLines: string[]
 
 /**
- * These tests ensure that secrets are handled properly
- * There should be a file .env that contains simple-comment secrets
- * such as ADMIN_PASSWORD and MONGO_DB_KEY
+ * Simple Comment uses a .env file to keep secrets
+ * It is not tracked in git
+ * example.env is tracked in git and shows the
+ * environmental variables expected in git
  * 
- * See example.env for an example 
- * BUT! and this is important: do not use the same values
- * as in example.env
+ * This test file ensures that:
+ * - `example.env` exists
+ * - `.env` exists
+ * - every variable in `example.env` is an environmental variable
+ * - each value is not the default value given in `example.env`
  **/
 
-describe("check each line", () => {
+describe("Ensures secrets are secret", () => {
+
+  test("example.env exists", () => {
+    const exampleEnvExists = fs.existsSync(`${process.cwd()}/example.env`)
+    expect(exampleEnvExists).toBe(true)
+  })
+  
   const exampleEnv = fs.readFileSync(`${process.cwd()}/example.env`, "utf8")
-  lines = exampleEnv.replace(/\r/g, "\n").replace(/\n{2}/g, "/n").split("\n")
+
+  exampleLines = exampleEnv
+    .replace(/\r/g, "\n")
+    .replace(/\n{2}/g, "/n")
+    .split("\n")
+    .map(l => l.trim())
+    .filter(l => !l.startsWith("#"))
 
   test(".env exists", () => {
     const envExists = fs.existsSync(`${process.cwd()}/.env`)
@@ -24,10 +39,10 @@ describe("check each line", () => {
   })
 
   test("example.env has information", () => {
-    expect(lines.length).toBeGreaterThan(0)
+    expect(exampleLines.length).toBeGreaterThan(0)
   })
 
-  lines.forEach(line => {
+  exampleLines.forEach(line => {
     const [varName, varValue] = line.split("=")
     test(`${varName} is defined as an environmental variable`, () => { expect(process.env[varName]).toBeDefined() })
     test(`${varName} is not ${varValue}`, () => { expect(process.env[varName]).not.toBe(varValue) })
