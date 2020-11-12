867cc1fa53343682ee1e1cda2a7bfef36d2c1124
Rendall <rendall@gmail.com>
2020-11-12 22:12:59 +0200

    Add login client script

--- src/lib/crypt.ts
+++ src/lib/crypt.ts
@@ -1,22 +1,18 @@
 import { v4 } from "uuid";
-import * as bcrypt from "bcrypt" 
+import * as crypto from 'crypto'
 import * as jwt from 'jsonwebtoken'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const saltRounds = 10
 const DAY = 60 * 24
 
 // Rounds down to the nearest second before adding `minutes`
 const getExpirationTime = (minutes:number):number => new Date(Math.floor(new Date().valueOf() / 10000) * 10000 + minutes * 60 * 1000).valueOf()
 
-export const hashPassword = (plainTextPassword: string) => bcrypt
-  .genSalt(saltRounds)
-  .then(salt => bcrypt.hash(plainTextPassword, salt))
+export const hashPassword = (password:string) => crypto.createHmac('sha256', process.env.HASH_SECRET).update(password).digest('hex')
 
-export const comparePassword = (plainTextPassword: string, hash: string) => bcrypt
-  .compare(plainTextPassword, hash)
+export const comparePassword = (plainTextPassword: string, hash: string) => hashPassword(plainTextPassword) === hash
 
 export const getAuthToken = (user:string, exp:number = getExpirationTime(DAY)) => jwt.sign({user, exp}, process.env.JWT_SECRET)
 
