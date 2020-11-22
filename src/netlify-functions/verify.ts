5a19221832f58f6b3ec4544291f1c4317f0159e0
Rendall <rendall@gmail.com>
2020-11-22 16:50:36 +0200

    Update `verify` endpoint to be aligned with others

--- src/netlify-functions/verify.ts
+++ src/netlify-functions/verify.ts
@@ -2,6 +2,8 @@ import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda"
 import * as jwt from "jsonwebtoken"
 import * as dotenv from "dotenv"
 import {
+  getAllowedOrigins,
+  getAllowOriginHeaders,
   getAuthCredentials,
   getAuthHeaderValue,
   getCookieToken,
@@ -9,83 +11,87 @@ import {
   hasTokenCookie,
   REALM
 } from "../lib/utilities"
+import { Success, TokenClaim, Error } from "../lib/simple-comment"
+import {
+  error404TopicNotFound,
+  error405MethodNotAllowed,
+  success204NoContent
+} from "../lib/messages"
+import { MongodbService } from "../lib/MongodbService"
 
 dotenv.config()
 
-export const handler: Handler = (
-  event: APIGatewayEvent,
-  context: Context,
-  callback: Callback
-) => {
-  switch (event.httpMethod.toUpperCase()) {
-    case "GET":
-      handleGet(event, callback)
-      break
-    default:
-      callback(
-        null,
-        buildResponse(`Method ${event.httpMethod} not supported`, 405)
-      )
-      break
+const service: MongodbService = new MongodbService(
+  process.env.DB_CONNECTION_STRING,
+  process.env.DATABASE_NAME
+)
+
+const getAllowHeaders = (event: APIGatewayEvent) => {
+  const allowedMethods = {
+    "Access-Control-Allow-Methods": "GET,OPTION"
   }
+  const allowedOriginHeaders = getAllowOriginHeaders(
+    event.headers,
+    getAllowedOrigins()
+  )
+  const headers = { ...allowedMethods, ...allowedOriginHeaders }
+  return headers
 }
 
-const handleGet = (event: APIGatewayEvent, callback: Callback) => {
+export const handler: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+  const dirs = event.path.split("/")
+  const isValidPath = dirs.length <= 5
+  const headers = getAllowHeaders(event)
+
+  if (!isValidPath)
+    return { ...error404TopicNotFound, body: `${event.path} is not valid` }
+
   const isBearer = hasBearerScheme(event.headers)
   const hasCookie = hasTokenCookie(event.headers)
 
-  if (!isBearer && !hasCookie)
-    callback(
-      null,
-      buildResponse(`Unauthenticated`, 401, {
-        ...HEADERS
-        // ...{ "WWW-Authenticate": `Basic realm="${REALM}", charset="UTF-8"` }
-      })
-    )
-  else {
+  if (!isBearer && !hasCookie) {
+    return {
+      ...success204NoContent,
+      body: "No Cookie header 'simple-comment-token' value"
+    }
+  }
+
+  const handleMethod = (
+    method
+  ): Promise<Success<TokenClaim> | Success | Error> => {
+    // This is going to throw an error. q.v. https://github.com/auth0/node-jsonwebtoken#errors--codes
     const token = hasCookie
       ? getCookieToken(event.headers)
       : getAuthCredentials(getAuthHeaderValue(event.headers))
 
-    try {
-      callback(
-        null,
-        buildResponse(
-          JSON.stringify(jwt.verify(token, process.env.JWT_SECRET)),
-          200
+    switch (method) {
+      case "GET":
+        return service.verifyGET(token)
+      case "OPTION":
+        return new Promise<Success>(resolve =>
+          resolve({ ...success204NoContent, headers })
         )
-      )
-    } catch (error) {
-      console.error(error)
-      switch (error.name) {
-        case "TokenExpiredError":
-          const EXPIRED_AUTHENTICATE_HEADER = {
-            "WWW-Authenticate": `Basic realm="${REALM}", error="invalid_token", error_description="The access token expired at ${error.expiredAt}", charset="UTF-8"`
-          }
-          callback(
-            null,
-            buildResponse(`token expired at ${error.expiredAt}`, 403, {
-              ...HEADERS,
-              ...EXPIRED_AUTHENTICATE_HEADER
-            })
-          )
-          break
-
-        default:
-          callback(null, buildResponse(error.message, 500))
-          break
-      }
+      default:
+        return new Promise<Error>(resolve => resolve(error405MethodNotAllowed))
     }
   }
-}
 
-const HEADERS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET"
-}
+  const convert = (res: { statusCode: number; body: any }) =>
+    res.statusCode === 204
+      ? { ...res, headers }
+      : {
+          ...res,
+          body: JSON.stringify(res.body),
+          headers
+        }
 
-export const buildResponse = (
-  body: string,
-  statusCode: number = 200,
-  headers = HEADERS
-) => ({ body, statusCode, headers })
+  try {
+    const response = await handleMethod(event.httpMethod)
+    return convert(response)
+  } catch (error) {
+    return error
+  }
+}
