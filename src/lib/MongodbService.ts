ba1e3efcc696318aadf8c888aab5f90d4bd2875c
Rendall <rendall@gmail.com>
2020-12-28 07:49:42 +0200

    Set SameSite to None at auth delete (log out)

--- src/lib/MongodbService.ts
+++ src/lib/MongodbService.ts
@@ -1456,7 +1456,7 @@ export class MongodbService extends Service {
     new Promise<Success>((resolve, reject) => {
       const pastDate = new Date(0).toUTCString()
       const COOKIE_HEADER = {
-        "Set-Cookie": `simple_comment_token=logged-out; path=/; HttpOnly; Expires=${pastDate}; SameSite${
+        "Set-Cookie": `simple_comment_token=logged-out; path=/; HttpOnly; Expires=${pastDate}; SameSite=None${
           this.isProduction ? "; Secure" : ""
         }`
       }
