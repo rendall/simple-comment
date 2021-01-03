c13efcf154e1d9fd3d5a1e3e943403ce6f5135f2
Rendall <rendall@gmail.com>
2021-01-03 15:07:17 +0200

    Hide user auth

--- src/dist/js/simple-comment.ts
+++ src/dist/js/simple-comment.ts
@@ -701,39 +701,47 @@ const setup = async (
     simpleCommentArea.appendChild(createdDiscussionDiv)
   }
 
-  const userDisplay = document.createElement("div")
-  userDisplay.setAttribute("id", "user-display")
-  simpleCommentArea.appendChild(userDisplay)
-  userDisplay.innerHTML = `
-    <p id="login-pulser">Logging in... Please wait</p>
-    <input type="hidden" id="claim-user">
-    <p id="user-name">Logged in as: Rendall (guest)</p>
-    <button id="log-out-button">Log out</button>
-    <div id="log-in-form">
-      <label for="userid">Username:</label>
-      <input id="userid" required="">
-      <label for="password">Password:</label>
-      <input id="password" type="password" required="">
-      <button id="log-in-button">Log in</button>
-    </div>
-    <div class="sign-up-form">
-      <p>Sign up for Simple Comment</p>
-      <p id="sign-up-status">&nbsp;</p>
-      <label for="sign-up-userid">Username:</label>
-      <input id="sign-up-userid" required="">
-      <label for="sign-up-password">Password:</label>
-      <input id="sign-up-password" type="password" required="">
-      <label for="sign-up-password-2">Same Password:</label>
-      <input id="sign-up-password-2" type="password" required="">
-      <label for="sign-up-email">Email:</label>
-      <input id="sign-up-email" required="">
-      <label for="sign-up-display-name">Display Name:</label>
-      <input id="sign-up-display-name" placeholder="How do you want to be known?">
-      <button id="sign-up-button">Sign up</button>
-  </div>`
+  const claimUserInput = document.createElement("input") as HTMLInputElement
+  claimUserInput.setAttribute("id", "claim-user")
+  claimUserInput.setAttribute("type", "hidden")
+  simpleCommentArea.appendChild(claimUserInput)
 
-  setupUserLogin()
-  setupSignup()
+  //   <input type="hidden" id="claim-user">
+
+  
+  // const userDisplay = document.createElement("div")
+  // userDisplay.setAttribute("id", "user-display")
+  // simpleCommentArea.appendChild(userDisplay)
+  // userDisplay.innerHTML = `
+  //   <p id="login-pulser">Logging in... Please wait</p>
+  //   <input type="hidden" id="claim-user">
+  //   <p id="user-name">Logged in as: Rendall (guest)</p>
+  //   <button id="log-out-button">Log out</button>
+  //   <div id="log-in-form">
+  //     <label for="userid">Username:</label>
+  //     <input id="userid" required="">
+  //     <label for="password">Password:</label>
+  //     <input id="password" type="password" required="">
+  //     <button id="log-in-button">Log in</button>
+  //   </div>
+  //   <div class="sign-up-form">
+  //     <p>Sign up for Simple Comment</p>
+  //     <p id="sign-up-status">&nbsp;</p>
+  //     <label for="sign-up-userid">Username:</label>
+  //     <input id="sign-up-userid" required="">
+  //     <label for="sign-up-password">Password:</label>
+  //     <input id="sign-up-password" type="password" required="">
+  //     <label for="sign-up-password-2">Same Password:</label>
+  //     <input id="sign-up-password-2" type="password" required="">
+  //     <label for="sign-up-email">Email:</label>
+  //     <input id="sign-up-email" required="">
+  //     <label for="sign-up-display-name">Display Name:</label>
+  //     <input id="sign-up-display-name" placeholder="How do you want to be known?">
+  //     <button id="sign-up-button">Sign up</button>
+  // </div>`
+
+  // setupUserLogin()
+  // setupSignup()
 
   const statusDisplay = document.createElement("p")
   statusDisplay.setAttribute("id", "status-display")
