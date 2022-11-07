3ab30ae4fcddd0577ee8f9d4f4804882474e0e72
Rendall <rendall@gmail.com>
2022-11-07 15:41:37 +0200

    Update cypress timing

--- cypress/integration/generic/input-validation.js
+++ cypress/integration/generic/input-validation.js
@@ -10,7 +10,7 @@ context("Input invalid data", () => {
 
   it("Submit a comment without adding email or name", () => {
     cy.wait(2500)
-    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me. The home for our two bookstores is not done, there is still stuff to move out. I have also taken over the store"s support department as well. I have never done these two things at the same time. Nor am I experienced in doing either. In fact, I am not certain how to do the support job. So I am looking for advice on how to do it.`
+    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me.`
     cy.get("#reply-field").type(comment)
     cy.get("#reply-submit-button").click()
     // This error should present immediately
@@ -22,7 +22,7 @@ context("Input invalid data", () => {
     cy.intercept('POST', '.netlify/functions/comment/').as('postComment')
     cy.wait(2500)
     cy.get("#name-field").type("Elli Reko Rautio")
-    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me. The home for our two bookstores is not done, there is still stuff to move out. I have also taken over the store"s support department as well. I have never done these two things at the same time. Nor am I experienced in doing either. In fact, I am not certain how to do the support job. So I am looking for advice on how to do it.`
+    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me.`
     cy.get("#reply-field").type(comment)
     cy.get("#reply-submit-button").click()
     cy.get("#status-display").should('have.class', 'error')
@@ -31,7 +31,7 @@ context("Input invalid data", () => {
   it("Submit a comment only adding email", () => {
     cy.wait(2500)
     cy.get("#email-field").type("fake@email.com")
-    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me. The home for our two bookstores is not done, there is still stuff to move out. I have also taken over the store"s support department as well. I have never done these two things at the same time. Nor am I experienced in doing either. In fact, I am not certain how to do the support job. So I am looking for advice on how to do it.`
+    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me.`
     cy.get("#reply-field").type(comment)
     cy.get("#reply-submit-button").click()
     cy.get("#status-display").should('have.class', 'error')
@@ -42,7 +42,7 @@ context("Input invalid data", () => {
     cy.wait(2500)
     cy.get("#email-field").type("invalid@emailcom")
     cy.get("#name-field").type("Elli Reko Rautio")
-    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me. The home for our two bookstores is not done, there is still stuff to move out. I have also taken over the store"s support department as well. I have never done these two things at the same time. Nor am I experienced in doing either. In fact, I am not certain how to do the support job. So I am looking for advice on how to do it.`
+    const comment = `I'd like to ask advice about how to tackle the upcoming work week. I am in the process of moving. The company moved the floorplan, I am taking my office with me.`
     cy.get("#reply-field").type(comment)
     cy.get("#reply-submit-button").click()
     cy.get(".error#status-display")
