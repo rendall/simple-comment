c1de34e630c2d3ca75b2508e89f3d4f99a9383cc
Rendall <rendall@gmail.com>
2022-11-21 13:52:09 +0200

    Add Svelte hello-world

---
+++ src/dist/hello.svelte
@@ -0,0 +1,19 @@
+<!--
+ @component
+ Here's some documentation for this component. It will show up on hover for
+ JavaScript/TypeScript projects using a LSP-compatible editor such as VSCode or
+ Vim/Neovim with coc.nvim.
+
+ - You can use markdown here.
+ - You can use code blocks here.
+ - JSDoc/TSDoc will be respected by LSP-compatible editors.
+ - Indentation will be respected as much as possible.
+-->
+
+<!-- @component You can use a single line, too -->
+
+<!-- @component But only the last documentation comment will be used -->
+
+<main>
+    <h1>Hello world</h1>
+</main>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require("path")
 const webpack = require("webpack")
 const dotenv = require("dotenv")
+const miniCssExtractPlugin = require("mini-css-extract-plugin")
 dotenv.config({
   path: path.join(__dirname, ".env")
 })
-
+const mode = process.env.NODE_ENV || "development"
 module.exports = {
-  mode: "production",
+  mode,
   entry: {
     "simple-comment": path.resolve(__dirname, "src/dist/js/simple-comment.ts"),
+    "hello": path.resolve(__dirname, "src/dist/hello.svelte"),
     "all-comments": path.resolve(__dirname, "src/dist/js/all-comments.ts")
   },
   devtool: "inline-source-map",
@@ -17,10 +19,41 @@ module.exports = {
     path: path.resolve(__dirname, "dist/js")
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    alias: {
+      svelte: path.resolve("node_modules", "svelte")
+    },
+    extensions: [".ts", ".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"]
   },
   module: {
-    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
+    rules: [
+      { test: /\.ts$/, use: ["ts-loader"] },
+      {
+        test: /\.css$/,
+        use: [
+          miniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false // necessary if you use url('/path/to/some/asset.png|jpg|gif')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html|svelte)$/,
+        use: "svelte-loader"
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+
+        // q.v. https://github.com/sveltejs/template-webpack/blob/master/webpack.config.js
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: { fullySpecified: false }
+      }
+    ]
   },
-  plugins: [new webpack.EnvironmentPlugin(["SIMPLE_COMMENT_API_URL"])]
+  plugins: [
+    new webpack.EnvironmentPlugin(["SIMPLE_COMMENT_API_URL"]),
+    new miniCssExtractPlugin({ filename: "[name].css" })
+  ]
 }
