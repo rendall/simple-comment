31925a872975b32ef220ad3e193819d96f0f7507
Rendall <rendall@gmail.com>
2022-11-25 16:58:35 +0200

    Format .js files (#73)

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-const path = require("path");
+const path = require("path")
 const webpack = require("webpack")
-const dotenv = require('dotenv');
+const dotenv = require("dotenv")
 dotenv.config({
-  path: path.join(__dirname, '.env')
+  path: path.join(__dirname, ".env")
 })
 
 module.exports = {
   mode: "production",
-  entry: { "simple-comment": path.resolve(__dirname, "src/dist/js/simple-comment.ts"), "all-comments": path.resolve(__dirname, "src/dist/js/all-comments.ts") },
+  entry: {
+    "simple-comment": path.resolve(__dirname, "src/dist/js/simple-comment.ts"),
+    "all-comments": path.resolve(__dirname, "src/dist/js/all-comments.ts")
+  },
   devtool: "inline-source-map",
   output: {
-    filename: '[name].js',
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist/js")
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
   module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
   },
-  plugins: [
-    new webpack.EnvironmentPlugin(['SIMPLE_COMMENT_API_URL'])
-  ]
-};
+  plugins: [new webpack.EnvironmentPlugin(["SIMPLE_COMMENT_API_URL"])]
+}
