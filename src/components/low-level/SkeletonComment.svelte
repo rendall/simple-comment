796122642795272e45eb69e51d44fa3dc126af6f
Rendall <rendall@gmail.com>
2023-08-18 14:30:07 +0300

    Add skeleton/loading components (#90)

---
+++ src/components/low-level/SkeletonComment.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import SkeletonText from "./SkeletonText.svelte"
+  export let isHidden = false
+</script>
+
+<div class="skeleton-comment" class:is-hidden={isHidden}>
+  <header class="comment-header">
+    <div class="user-avatar"><SkeletonText avatar /></div>
+    <div class="comment-info">
+      <p class="user-name"><SkeletonText width="8rem" /></p>
+      <p class="comment-date"><SkeletonText width="12rem" /></p>
+    </div>
+  </header>
+  <article class="comment-body">
+    <SkeletonText paragraph />
+  </article>
+</div>
+
+<style lang="scss">
+  .comment-header {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 0.25rem;
+    min-height: unset;
+
+    p {
+      margin: 0;
+      padding: 0;
+    }
+
+    .user-avatar {
+      flex: 0 0 3rem;
+      margin-right: 0.9rem;
+      display: inline-flex;
+      max-width: 2.5rem;
+      border-radius: 50%;
+      overflow: hidden;
+    }
+
+    .comment-info {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      width: calc(100% - 3rem - 1.3rem);
+
+      .user-name {
+        margin-right: 1rem;
+      }
+    }
+  }
+
+  .comment-body {
+    margin-left: 0.5rem;
+    padding-left: 2rem;
+  }
+</style>
